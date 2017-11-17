f810d524b5e145aba0afbf742ab200913d7bfac4
ingofreak <33750816+ingofreak@users.noreply.github.com>
2017-11-17 11:57:31 +0000

    Add files via upload
    nagdyr, seinni

--- Lokaverkefni.py
+++ Lokaverkefni.py
@@ -51,7 +51,34 @@ print(rotta2.tegund,rotta2.afl,rotta3.stadur)
 print(rotta3.tegund,rotta3.afl,rotta3.stadur)
 print(hamstur.tegund,hamstur.afl,hamstur.stadur)
 
-#kasta= random.randint()
+svar=0
+while svar!="2":
+    print("-----Nagdýr-----")
+    print("1 - Spila")
+    print("2 - Hætta")
+    print("-----------------")
+    svar=input("Sláðu inn tölu frá bilinu 1-2 ")
+
+    if svar=="1":
+        svar1=0
+        while svar1!="3":
+            print("\n-----Valmynd-----")
+            print("1 - Kasta teningi?")
+            print("2 - Staðsetning?")
+            print("3 - Hætta")
+            svar1=input("Sláðu inn tölu frá bilinu 1-3 ")
+
+
+
+            if svar1=="1":
+                print("\nkastað er teningi ")
+                teningur = random.randint(1, 6)
+                print("þú fékkst =", teningur)
+                player1.stadur=player1.stadur + teningur
+
+            elif    svar1=="2":
+                print(player1.stadur)
+
 
 
 
