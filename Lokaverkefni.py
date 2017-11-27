fb3968e14070218a17f1dcbce2651f2116c38aed
ingofreak <33750816+ingofreak@users.noreply.github.com>
2017-11-27 14:34:32 +0000

    Update Lokaverkefni.py

--- Lokaverkefni.py
+++ Lokaverkefni.py
@@ -6,63 +6,33 @@ import random
 
 
 class Nagdyr:
-    #stadur=0
-    #afl=random.randint(0,6)
 
-    def __init__(self,tegund,stadur,afl):
+
+    def __init__(self,tegund,stadur,afl,þyngd,tennur):
         self.tegund=tegund
         self.afl=afl
         self.stadur=stadur
-
-    #def stadur(self):
-     #   if stadur <= 100:
-      #      print("Þú vinnur")
-       # else:
-        #    print("Þú er á reit",stadur)
-
-    #def afl(self):
-     #   self.afl=random.randint(0,6,2)
-
-    #def rottu(self):
-        #self.stadur=random.randint(1,100)
-        #self.afl=random.randint(0,6,2)
+        self.þyngd=þyngd
+        self.tennur=tennur
 
 
-   # def hamstur(self):
-    #    self.stadur=random.randint(1,100)
-     #   self.afl = random.randint(0,6,2)
-
-    #def mus(self):
-        #self.stadur=0
-        #self.afl = random.randint(0, 6, 2)
-
-
-#stadur=0
-#afl=0
-
-#player1 = Nagdyr("Mús",0, random.randrange(2,7,2))
-#rotta1 = Nagdyr("Rotta",random.randrange(1,100), random.randrange(2,7,2))
-#rotta2 = Nagdyr("Rotta",random.randrange(1,100), random.randrange(2,7,2))
-#rotta3 = Nagdyr("Rotta",random.randrange(1,100), random.randrange(2,7,2))
-#hamstur = Nagdyr("Hamstur",random.randrange(1,100), random.randrange(2,7,2))
-
-#print(player1.tegund,player1.afl,player1.stadur)
-#print(rotta1.tegund,rotta1.afl,rotta1.stadur)
-#print(rotta2.tegund,rotta2.afl,rotta2.stadur)
-#print(rotta3.tegund,rotta3.afl,rotta3.stadur)
-#print(hamstur.tegund,hamstur.afl,hamstur.stadur)
 
 svar=0
 while svar!="2":
-    player1 = Nagdyr("Mús", 0, random.randrange(2, 7, 2))
-    rotta1 = Nagdyr("Rotta", random.randrange(1, 100), random.randrange(2, 7, 2))
-    rotta2 = Nagdyr("Rotta", random.randrange(1, 100), random.randrange(2, 7, 2))
-    rotta3 = Nagdyr("Rotta", random.randrange(1, 100), random.randrange(2, 7, 2))
-    hamstur = Nagdyr("Hamstur", random.randrange(1, 50), random.randrange(2, 7, 2))
-    print(player1.tegund, player1.afl, player1.stadur)
-    print(rotta1.tegund, rotta1.afl, rotta1.stadur)
-    print(rotta2.tegund, rotta2.afl, rotta2.stadur)
-    print(rotta3.tegund, rotta3.afl, rotta3.stadur)
+    player1 = Nagdyr("Mús", 0, random.randrange(2, 7, 2),random.randint(1, 3),random.randrange(2, 7, 2))
+    rotta1 = Nagdyr("Rotta", random.randrange(1, 100), random.randrange(2, 7, 2),random.randint(1, 3),random.randrange(2, 7, 2))
+    rotta2 = Nagdyr("Rotta", random.randrange(1, 100), random.randrange(2, 7, 2),random.randint(1, 3),random.randrange(2, 7, 2))
+    rotta3 = Nagdyr("Rotta", random.randrange(1, 100), random.randrange(2, 7, 2),random.randint(1, 3),random.randrange(2, 7, 2))
+    hamstur = Nagdyr("Hamstur", random.randrange(1, 50), random.randrange(2, 7, 2),0,0)
+    # Hérna tökum við aflið, þyngdinna og hversu hvassar tennur og leggjum það saman í power
+    powermus = player1.afl + player1.þyngd + player1.tennur
+    powerrotta1 = rotta1.afl + rotta1.þyngd + rotta1.tennur
+    powerrotta2 = rotta2.afl + rotta2.þyngd + rotta2.tennur
+    powerrotta3 = rotta3.afl + rotta3.þyngd + rotta3.tennur
+    print(player1.tegund, player1.afl, player1.stadur,player1.þyngd,player1.tennur)
+    print(rotta1.tegund, rotta1.afl, rotta1.stadur,rotta1.þyngd,rotta1.tennur)
+    print(rotta2.tegund, rotta2.afl, rotta2.stadur,rotta2.þyngd,rotta2.tennur)
+    print(rotta3.tegund, rotta3.afl, rotta3.stadur,rotta3.þyngd,rotta3.tennur)
     print(hamstur.tegund, hamstur.afl, hamstur.stadur)
     print("-----Nagdýr-----")
     print("1 - Spila")
@@ -86,46 +56,45 @@ while svar!="2":
                 print("\nkastað er teningi ")
                 teningur = random.randint(1, 6)
                 print("þú fékkst =", teningur)
-                #player1.stadur=player1.stadur + teningur
-                #print("Þú ert á reit",player1.stadur)
 
-                #Tjekkar hvort músinn fer framhjá rottonum og hamstur
+
+
                 for x in range(player1.stadur, teningur+player1.stadur):
-                    #print("Músinn",player1.stadur)
-                    #print("rotta1",rotta1.stadur,"rotta2",rotta2.stadur,"rotta3",rotta3.stadur)
+
                     x=x+1
                     print(x)
-                    if rotta1.stadur == x: #or rotta2.stadur == x or rotta3.stadur == x:
-                        print("Þú ert á sama reit og rotta1")
-                        if rotta1.afl > player1.afl:
-                            print("\nRotta vinnur\n")
+                    if rotta1.stadur == x:
+
+                        print("Þú ert á sama reit og rotta 1")
+                        if powerrotta1 > powermus:
+                            print("\nRottan vinnur\n")
                             player1.stadur = player1.stadur - teningur - rotta1.afl
                             print("Þú ferð til baka um",rotta1.afl,"marga reiti")
-                        elif rotta1.afl < player1.afl:
+                        elif powerrotta1 < powermus:
                             print("\nPlayer 1 vinnur\n")
                         else:
                             print("Jafntefli")
 
                         #athuga hvort músin eða rottan hefur meira afl
                     elif rotta2.stadur == x:
-                        print("Þú ert á sama reit og rotta2")
-                        if rotta2.afl > player1.afl:
-                            print("\nRotta vinnur")
+                        print("Þú ert á sama reit og rotta 2")
+                        if powerrotta2 > powermus:
+                            print("\nRottan vinnur\n")
                             player1.stadur = player1.stadur - teningur - rotta2.afl
                             print("Þú ferð til baka um", rotta2.afl, "marga reiti")
-                        elif rotta2.afl < player1.afl:
-                            print("\nPlayer 1 vinnur")
+                        elif powerrotta2 < powermus:
+                            print("\nPlayer 1 vinnur\n")
                         else:
                             print("Jafntefli")
 
                     elif rotta3.stadur == x:
-                        print("Þú ert á sama reit og rotta3")
-                        if rotta3.afl > player1.afl:
-                            print("\nRotta vinnur")
-                            player1.stadur=player1.stadur-teningur-rotta3.afl
+                        print("Þú ert á sama reit og rotta 3")
+                        if powerrotta3 > powermus:
+                            print("\nRottan vinnur\n")
+                            player1.stadur = player1.stadur - teningur - rotta3.afl
                             print("Þú ferð til baka um", rotta3.afl, "marga reiti")
-                        elif rotta3.afl < player1.afl:
-                            print("\nPlayer 1 vinnur")
+                        elif powerrotta3 < powermus:
+                            print("\nPlayer 1 vinnur\n")
                         else:
                             print("Jafntefli")
 
@@ -135,6 +104,8 @@ while svar!="2":
                         print("\n Þú lentir á reit",player1.stadur)
 
                 player1.stadur = player1.stadur + teningur
+                if player1.stadur < 0:
+                    player1.stadur = 0
                 print("Þú ert á reit", player1.stadur)
 
                 teningur = random.randint(1, 6)
@@ -146,12 +117,12 @@ while svar!="2":
                         x=x+1
                         if player1.stadur == x:
                             print("Rotta 1 hittir mús")
-                            if rotta1.afl > player1.afl:
-                                print("\nRotta vinnur")
+                            if powerrotta1 > powermus:
+                                print("\nRotta 1 vinnur")
                                 player1.stadur = player1.stadur - rotta1.afl
                                 print("Þú ferð til baka um", rotta1.afl, "marga reiti")
-                            elif rotta1.afl < player1.afl:
-                                print("\nPlayer 1 vinnur")
+                            elif powerrotta1 < powermus:
+                                print("\nMúsin vinnur")
                                 player1.stadur=player1.stadur + 2
                             else:
                                 print("Jafntefli")
@@ -164,12 +135,12 @@ while svar!="2":
                         x=x-1
                         if player1.stadur == x:
                             print("Rotta 1 hittir mús")
-                            if rotta1.afl > player1.afl:
-                                print("\nRotta vinnur")
+                            if powerrotta1 > powermus:
+                                print("\nRotta 1 vinnur")
                                 player1.stadur = player1.stadur - rotta1.afl
                                 print("Þú ferð til baka um", rotta1.afl, "marga reiti")
-                            elif rotta1.afl < player1.afl:
-                                print("\nPlayer 1 vinnur")
+                            elif powerrotta1 < powermus:
+                                print("\nMúsin vinnur")
                                 player1.stadur=player1.stadur + 2
                             else:
                                 print("Jafntefli")
@@ -184,12 +155,12 @@ while svar!="2":
                         x=x+1
                         if player1.stadur == x:
                             print("Rotta 2 hittir mús")
-                            if rotta2.afl > player1.afl:
-                                print("\nRotta vinnur")
+                            if powerrotta2 > powermus:
+                                print("\nRotta 2 vinnur")
                                 player1.stadur = player1.stadur - rotta2.afl
                                 print("Þú ferð til baka um", rotta2.afl, "marga reiti")
-                            elif rotta1.afl < player1.afl:
-                                print("\nPlayer 1 vinnur")
+                            elif powerrotta2 < powermus:
+                                print("\nMúsin vinnur")
                                 player1.stadur=player1.stadur + 2
                             else:
                                 print("Jafntefli")
@@ -201,12 +172,12 @@ while svar!="2":
                         x=x-1
                         if player1.stadur == x:
                             print("Rotta 2 hittir mús")
-                            if rotta2.afl > player1.afl:
-                                print("\nRotta vinnur")
+                            if powerrotta2 > powermus:
+                                print("\nRotta 2 vinnur")
                                 player1.stadur = player1.stadur - rotta2.afl
                                 print("Þú ferð til baka um", rotta2.afl, "marga reiti")
-                            elif rotta2.afl < player1.afl:
-                                print("\nPlayer 1 vinnur")
+                            elif powerrotta2 < powermus:
+                                print("\nMúsin vinnur")
                                 player1.stadur=player1.stadur + 2
                             else:
                                 print("Jafntefli")
@@ -221,12 +192,12 @@ while svar!="2":
                         x=x+1
                         if player1.stadur == x:
                             print("Rotta 3 hittir mús")
-                            if rotta3.afl > player1.afl:
-                                print("\nRotta vinnur")
+                            if powerrotta3 > powermus:
+                                print("\nRotta 3 vinnur")
                                 player1.stadur = player1.stadur - rotta3.afl
                                 print("Þú ferð til baka um", rotta3.afl, "marga reiti")
-                            elif rotta3.afl < player1.afl:
-                                print("\nPlayer 1 vinnur")
+                            elif powerrotta3 < powermus:
+                                print("\nMúsin vinnur")
                                 player1.stadur=player1.stadur + 2
                             else:
                                 print("Jafntefli")
@@ -238,11 +209,11 @@ while svar!="2":
                         x=x-1
                         if player1.stadur == x:
                             print("Rotta 1 hittir mús")
-                            if rotta3.afl > player1.afl:
+                            if powerrotta3 > powermus:
                                 print("\nRotta vinnur")
                                 player1.stadur = player1.stadur - rotta3.afl
                                 print("Þú ferð til baka um", rotta3.afl, "marga reiti")
-                            elif rotta3.afl < player1.afl:
+                            elif powerrotta3 < powermus:
                                 print("\nPlayer 1 vinnur")
                                 player1.stadur=player1.stadur + 2
                             else:
@@ -252,7 +223,10 @@ while svar!="2":
 
                 teningur = random.randint(1, 6)
                 print("Núna kastar hamsturinn")
+                if player1.stadur < 0:
+                    player1.stadur = 0
                 print("Hamsturinn fær",teningur)
+                #Hvort hamsturinn fer áfram eða afturábak að músinni
                 if player1.stadur > hamstur.stadur:
                 #Hvort að hamstur fer framhjá mús
                     for x in range(hamstur.stadur,hamstur.stadur+teningur):
@@ -310,7 +284,6 @@ while svar!="2":
                 elif player1.stadur < hamstur.stadur:
                 #Hvort að hamstur fer framhjá mús
                     for x in range(hamstur.stadur,hamstur.stadur-teningur,-1):
-                        #x=x
                         print(x)
                         if player1.stadur == x:
                             print("HAMSTURINN KASTAR ÞÉR ÁFRAM!")
@@ -367,9 +340,33 @@ while svar!="2":
             elif    svar1=="2":
 
                 print("Þú ert á reit",player1.stadur)
-                print("Rotta1",rotta1.stadur)
-                print("Rotta2", rotta2.stadur)
-                print("Rotta3", rotta3.stadur)
+                if player1.tennur == 2:
+                    print("Þú hefur ekki hvassar tennur.")
+                elif player1.tennur == 4:
+                    print("Þú ert með hvassar tennur.")
+                elif player1.tennur == 6:
+                    print("Þú ert með MJÖG hvassar tennur.")
+                print("Rotta 1",rotta1.stadur)
+                if rotta1.tennur == 2:
+                    print("Rotta 1 hefur ekki hvassar tennur.")
+                elif rotta1.tennur == 4:
+                    print("Rotta 1 er með hvassar tennur.")
+                elif rotta1.tennur == 6:
+                    print("Rotta 1 er með MJÖG hvassar tennur.")
+                print("Rotta 2", rotta2.stadur)
+                if rotta2.tennur == 2:
+                    print("Rotta 2 hefur ekki hvassar tennur.")
+                elif rotta2.tennur == 4:
+                    print("Rotta 2 er með hvassar tennur.")
+                elif rotta2.tennur == 6:
+                    print("Rotta 2 er með MJÖG hvassar tennur.")
+                print("Rotta 3", rotta3.stadur)
+                if rotta3.tennur == 2:
+                    print("Rotta 3 hefur ekki hvassar tennur.")
+                elif rotta3.tennur == 4:
+                    print("Rotta 3 er með hvassar tennur.")
+                elif rotta3.tennur == 6:
+                    print("Rotta 3 er með MJÖG hvassar tennur.")
                 print("Hamstur", hamstur.stadur)
 
 
